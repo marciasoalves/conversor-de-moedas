8729a66e2afb0ea79aad776a33ff3dc3aa5dedd2
Márcia Alves <marciasoalves@gmail.com>
2021-10-05 15:01:20 -0300

    conversor de medidas
    conversor de medidas com angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ConversorModule } from './conversor';
+import {  HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ConversorModule,
+    HttpClientModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
---
+++ src/app/conversor/utils/index.ts
@@ -0,0 +1,3 @@
+export * from './modal-cotacao.component';
+
+
